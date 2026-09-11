5570329f7f46b1c57df41196d3fc0340b11fbe3a
TryhubaIvan <121.24tryhuba.i@nltu.lviv.ua>
2026-09-11 00:35:46 -0700

    initial commit

---
+++ TryhubaivanLab1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TryhubaivanLab1
+//
+//  Created by stud on 10.09.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "airplane")
+                .imageScale(.large)
+                .foregroundStyle(.black)
+            Text("Hello, world")
+                .font(.subheadline)
+                .bold(true)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TryhubaivanLab1/TryhubaivanLab1App.swift
@@ -0,0 +1,17 @@
+//
+//  TryhubaivanLab1App.swift
+//  TryhubaivanLab1
+//
+//  Created by stud on 10.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct TryhubaivanLab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
