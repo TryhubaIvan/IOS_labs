252f80893741291e890b2b7877f9f899053579ec
TryhubaIvan <121.24tryhuba.i@nltu.lviv.ua>
2026-09-24 23:31:33 -0700

    Initial commit

---
+++ TryhubaivanLab1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TryhubaivanLab1
+//
+//  Created by stud on 10.09.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "tree")
+                .imageScale(.large)
+                .foregroundStyle(.white)
+            Text("lab 1")
+                .font(.subheadline)
+                .bold(true)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
